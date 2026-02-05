3f1cc609760fc33dd722b860a67d9629ddd1af1a
briansill <briansill7@gmail.com>
2026-02-05 15:07:20 -0500

    Init and Add Banner

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Banner>
+        <div>Indoor Plant Caring Guide</div>
+      </Banner>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/Laura Loves Plants Logo_Draft_1.png"
+import {logo as logoClass} from "./Banner.module.css"
+
+const subtitleSytle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+};
+
+const Banner = ({children}) => {
+    return (
+        <header className="row mb-3">
+            <div className="col-5">
+                <img src={logo} className={logoClass} alt="logo" />
+            </div>
+            <div className="col-7 mt-5" style={subtitleSytle}>
+                {children}
+            </div>
+        </header>
+    )
+}
+
+export default Banner;
