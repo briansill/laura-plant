c79323e4940736b821723c2b0134d885a18605d7
briansill <briansill7@gmail.com>
2026-02-11 14:01:01 -0500

    finished errorBoundary impl

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import './App.css'
 import Plant from "./components/Plant"
 import PlantList from "./components/PlantList"
 import { useState } from "react"
+import ErrorBoundary from "./components/ErrorBoundary"
 
 function App() {
   const [selectedPlant, setSelectedPlant] = useState();
 
   return (
-    <>
+    <ErrorBoundary fallback="Something went wrong loading App.">
       <Banner>
         <div>Indoor Plant Caring Guide</div>
       </Banner>
       {selectedPlant ? <Plant plant={selectedPlant} />
         : <PlantList selectPlant={setSelectedPlant} />}
-    </>
+    </ErrorBoundary>
   )
 }
 
